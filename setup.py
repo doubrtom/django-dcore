5df6e7355b6fbf8d6ef3ea751db9c5c87570e1cd
Tomas Doubravsky <doubravskytomas@gmail.com>
2019-08-21 20:27:06 +0200

    Increase version.
    - tag: version, project

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 setup(
     name='django-dcore',
-    version='0.15',
+    version='0.16',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',

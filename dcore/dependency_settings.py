fb055535608774ba212d83e968680ac3feb91f38
Tomas Doubravsky <doubravskytomas@gmail.com>
2019-08-21 19:45:24 +0200

    Update friendly_errors settings.
    - tag: dependency_settings

--- dcore/dependency_settings.py
+++ dcore/dependency_settings.py
@@ -1,6 +1,16 @@
 FRIENDLY_ERRORS = {
     'FIELD_ERRORS': {
-        'CharField': {'password_incorrect': 2016, 'password_mismatch': 2017},
-        'ModelChoiceField': {'required': 2004, 'null': 2024, 'invalid_choice': 2081},
+        'CharField': {
+            'password_incorrect': 2016,
+            'password_mismatch': 2017,
+            'not_editable': 2501,
+            'invalid_token': 2502,
+            'token_expired': 2503,
+        },
+        'ModelChoiceField': {
+            'required': 2004,
+            'null': 2024,
+            'invalid_choice': 2081
+        },
     }
 }

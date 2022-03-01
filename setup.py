26d37b92a827c1a300f8ae95d07fdcd79ff8c998
Tomas Doubravsky <doubravskytomas@gmail.com>
2022-03-01 14:00:16 +0100

    Increase version - 0.34

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
 
 setup(
     name='django-dcore',
-    version='0.33',
+    version='0.34',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
